8ada49a308b2312b636cefe927b50d750ea763f1
peedrohcosta <pedro.costa@logauto.com.br>
2025-07-27 13:14:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 app.post('/save-google-token', async (req, res) => {
